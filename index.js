4b0a0e339daf9ab2eede575ad83e12bf69997e55
lionnoil682 <lionnoil682@gmail.com>
2024-09-19 16:04:07 +0900

    0919 back update afternoon

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'https://travel-planner-front.aicclionnoil.co.kr',
-    // origin: 'http://localhost:3000',
+    // origin: 'https://travel-planner-front.aicclionnoil.co.kr',
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
